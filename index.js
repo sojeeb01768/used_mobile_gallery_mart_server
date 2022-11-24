49e9017eaba0c8efe81bf0279d6ee655494ccf53
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-24 20:27:35 +0600

    added database and data

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
+require('dotenv').config();
 
 const app = express();
 
 // middleware
 app.use(cors())
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kvqywrf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('usedMobileGallery').collection('categories');
+        const productsCollection = client.db('usedMobileGallery').collection('products');
+
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const category = await categoriesCollection.find(query).toArray();
+            res.send(category);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const products = await productsCollection.findOne(query);
+            res.send(products)
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            query = { id: id };
+            const category = await productsCollection.find(query).toArray();
+            res.send(category);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log);
+
 
 app.get('/', async (req, res) => {
     res.send('UMG Mart is running')
